7e08005abc509e15d1d3e32c3d71f3a6aa2c7cf3
Chuma Nnaji <cnnaji2003@gmail.com>
2026-08-09 11:16:03 -0700

    Revert webpack config to known-good multi-target layout to avoid chunk filename conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
-const {version} = require('./package.json');
+const packageJson = require('./package.json');
 const CopyPlugin = require('copy-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
 
-const baseConfig = {
+const config = {
     entry: './es6/app.js',
     devtool: 'inline-source-map',
     mode: 'production',
@@ -27,7 +27,7 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: [
                     'style-loader',
-                    'css-loader',
+                    'css-loader'
                 ],
             },
             {
@@ -39,107 +39,87 @@ const baseConfig = {
                 ],
             },
         ],
-    },
+    }
 };
 
-const createConfig = (name, overrides = {}) => ({
-    ...baseConfig,
-    ...overrides,
-});
-
-const createCopyPatterns = (manifestTransform) => [
-    {from: 'html/start.html'},
-    {from: 'icons/*'},
-    {
-        from: 'manifest.template.json',
-        to: 'manifest.json',
-        transform: (content) => {
-            const manifest = JSON.parse(content.toString());
-            manifest.version = version;
-            if (manifestTransform) {
-                manifestTransform(manifest);
-            }
-            return JSON.stringify(manifest);
-        },
-    },
-];
-
-const chromeConfig = createConfig('chrome', {
+var chromeConfig = Object.assign({}, config, {
     output: {
         filename: 'metro-start.js',
-        chunkFilename: '[name].[contenthash].js',
         path: `${__dirname}/dist/chrome`,
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: 25,
-            minSize: 20000,
-        },
-        runtimeChunk: 'single',
-    },
     plugins: [
-        new CopyPlugin({patterns: createCopyPatterns()}),
+        new CopyPlugin({
+            patterns: [
+                {from: 'html/start.html'},
+                {from: 'icons/*' },
+                {
+                    from: 'manifest.template.json',
+                    to: 'manifest.json',
+                    transform(content) {
+                        let manifest = JSON.parse(content.toString());
+                        manifest.version = packageJson.version;
+                        return JSON.stringify(manifest);
+                    }
+                }]}),
         new ZipPlugin({
             path: `${__dirname}/dist`,
             filename: 'metro-start-chrome.zip',
-        }),
-    ],
+        })]
 });
 
-const firefoxConfig = createConfig('firefox', {
+var firefoxConfig = Object.assign({}, config, {
     output: {
         filename: 'metro-start.js',
         path: `${__dirname}/dist/firefox`,
     },
-    plugins: [
-        new CopyPlugin({
-            patterns: createCopyPatterns((manifest) => {
-                manifest.manifest_version = 2;
-                manifest.permissions.push(...(manifest.host_permissions || []));
-                delete manifest.host_permissions;
-                manifest.browser_specific_settings = {
-                    gecko: {
-                        id: 'metro-start@metro-start.com',
-                        strict_min_version: '77.0',
-                    },
-                };
-            }),
-        }),
-        new ZipPlugin({
-            path: `${__dirname}/dist`,
-            filename: 'metro-start-firefox.zip',
-        }),
-    ],
-});
+    plugins: [new CopyPlugin({
+        patterns: [
+            {from: 'html/start.html'},
+            {from: 'icons/*' },
+            {
+                from: 'manifest.template.json',
+                to: 'manifest.json',
+                transform(content) {
+                    let manifest = JSON.parse(content.toString());
+                    manifest.version = packageJson.version;
+                    manifest.manifest_version = 2;
+                    manifest.browser_specific_settings = {
+                        gecko: {
+                            id: 'metro-start@metro-start.com',
+                            strict_min_version: '77.0'
+                        }
+                    };
+                    return JSON.stringify(manifest);
+                }
+            }]
+    }),
+    new ZipPlugin({
+        path: `${__dirname}/dist`,
+        filename: 'metro-start-firefox.zip',
+    })]});
 
-const xcodeConfig = createConfig('xcode', {
+var xcodeConfig = Object.assign({}, config, {
     output: {
         filename: 'metro-start.js',
         path: `${__dirname}/dist/xcode`,
     },
-    plugins: [
-        new CopyPlugin({patterns: createCopyPatterns()}),
-        new ZipPlugin({
-            path: `${__dirname}/dist`,
-            filename: 'metro-start-xcode.zip',
-        }),
-    ],
-});
-
-module.exports = (env = {}) => {
-    // Allow selecting a specific target via --env target=chrome|firefox|xcode|all
-    const target = env.target || process.env.BUILD_TARGET || 'chrome';
-    switch (target) {
-        case 'chrome':
-            return chromeConfig;
-        case 'firefox':
-            return firefoxConfig;
-        case 'xcode':
-            return xcodeConfig;
-        case 'all':
-            return [chromeConfig, firefoxConfig, xcodeConfig];
-        default:
-            return chromeConfig;
-    }
-};
+    plugins: [new CopyPlugin({
+        patterns: [
+            {from: 'html/start.html'},
+            {from: 'icons/*' },
+            {
+                from: 'manifest.template.json',
+                to: 'manifest.json',
+                transform(content) {
+                    let manifest = JSON.parse(content.toString());
+                    manifest.version = packageJson.version;
+                    return JSON.stringify(manifest);
+                }
+            }]
+    }),
+    new ZipPlugin({
+        path: `${__dirname}/dist`,
+        filename: 'metro-start-xcode.zip',
+    })]});
+    
+module.exports = [chromeConfig, firefoxConfig, xcodeConfig];
