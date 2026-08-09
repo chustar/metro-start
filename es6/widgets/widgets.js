337c79ef23c28ed901cc90024e48a258aae5c13e
Chuma Nnaji <cnnaji2003@gmail.com>
2026-08-09 13:47:09 -0700

    Revert "Merge branch 'chustar-browser-test-site' of https://github.com/chustar/metro-start into chustar-browser-test-site"
    This reverts commit fe87607bd29217fe688aeaa4144896268a363c1d, reversing
    changes made to 786845925d3dcc547d61bb299d0e4f8071b340b0.

--- es6/widgets/widgets.js
+++ es6/widgets/widgets.js
@@ -1,40 +1,16 @@
 import weather from './weather';
+import themes from './themes';
 import about from './about';
-
-let _themesModule = null;
-
 export default {
     weather: weather,
+    themes: themes,
     about: about,
 
-    data: [weather, about],
+    data: [weather, themes, about],
 
     init: function(document) {
-        // Initialize light-weight or always-used widgets immediately.
         this.data.forEach((module) => {
             module.init(document);
         });
-
-        // Defer loading the themes module until the user attempts to open it.
-        const editBtn = document.getElementById('editThemeButton');
-        if (editBtn) {
-            const handler = async (e) => {
-                editBtn.removeEventListener('click', handler);
-                try {
-                    if (!_themesModule) {
-                        const mod = await import('./themes');
-                        _themesModule = mod.default || mod;
-                        _themesModule.init(document);
-                    }
-                    // After init, open the editor immediately.
-                    if (_themesModule && _themesModule.openThemeEditor) {
-                        _themesModule.openThemeEditor();
-                    }
-                } catch (err) {
-                    console.error('Failed to load themes module:', err);
-                }
-            };
-            editBtn.addEventListener('click', handler);
-        }
     },
 };
